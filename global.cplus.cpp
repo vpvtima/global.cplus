c9cbfdd7417367bdb01cda4633b8728332afe1e5
Vpv Tima <vpv.tima@gmail.com>
2017-09-08 15:10:54 +0300

    подключение файла pointer.cpp

--- global.cplus.cpp
+++ global.cplus.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "list.cpp"
+#include "pointer.cpp"
 
 int main() {
     setlocale(LC_ALL, "rus");
-    listMain();
+    pointerMain();
+    //listMain();
     //std::cout << "Hello, World!" << std::endl;
     system("pause");
     return 0;
