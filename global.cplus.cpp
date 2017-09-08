418dd158260f9a839dadc19e5d45de5aa96a7534
Vpv Tima <vpv.tima@gmail.com>
2017-09-08 15:37:18 +0300

    подключение класса class01.cpp

--- global.cplus.cpp
+++ global.cplus.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include "list.cpp"
 #include "pointer.cpp"
+#include "class01.cpp"
 
 int main() {
     setlocale(LC_ALL, "rus");
-    pointerMain();
+
+    // --- классы ---
+    Class01 objClass01(11,11,2011); // объявление объекта и инициализвция элементов данных
+    objClass01.message(); // вызов функции message
+    objClass01.getDate(); // отобразить дату
+
+    // --- указатели ---
+    //pointerMain();
+
+    // --- ссылки ---
     //listMain();
+
     //std::cout << "Hello, World!" << std::endl;
     system("pause");
     return 0;
